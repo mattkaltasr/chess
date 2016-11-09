d2342fc9c2b8864ee9f89e80636f1a8db49ec35a
mattkaltasr <mkalitasr@gmail.com>
2016-11-09 11:14:26 -0500

    worked on main try catch added Gamemove class to work with moves

--- Chess84/src/ChessGame/ChessGame.java
+++ Chess84/src/ChessGame/ChessGame.java
@@ -1,5 +1,105 @@
 package ChessGame;
 
+import java.util.Scanner;
+
+import game.Board;
+import game.TestBoard;
+import gamePieces.IllegalMoveException;
+import gamePieces.PieceColor;
+
 public class ChessGame {
+	public static void main(String[] args)throws IllegalMoveException {
+	 Boolean resign= false;
+	 Boolean player=true;//set for white if true 
+	 Scanner input =new Scanner(System.in);
+	 PieceColor gameColor;
+	int Gamecounter =0:
+	
+		
+		
+		//sets up game board for game play 
+	Board newGame= new Board();
+	board.initialize();
+	board.initializePieceCounts();
+	board.printBoard();
+	
+	while((getLastPawnmoveorcapture()<50)&&!board.draw()&&!TestBoard.isEnd()){
+		Gamecounter++;
+		
+		if(player){
+			gameColor=PieceColor.White;
+			System.out.print("\n White's Turn ");}//endif white 
+		else{ gameColor=PieceColor.Black;
+		System.out.print("\n Black's Turn ");
+			
+		}//end else 
+		
+		try{
+			String gameMove=input.nextLine();
+			TestBoard.updateProtectedSquares(newGame);
+			if(gameMove.equalsIgnoreCase("resign")){
+				if(gameColor==PieceColor.Black){
+					System.out.print("White Player  Wins!! ");
+				}//endif black 
+				else{
+					System.out.print("Black Player Wins!! ");
+				}//end else
+				break;
+			}//end gamemove test 
+			
+			
+			
+			gameMove=gameMove.trim();//cuts down whitespace for begining and end of string 
+			
+			if(gameMove.length()==7) {
+				TestBoard.promotePawn(gameMove,newGame,gameColor);}
+			else { movePiece(gameMove,newBoard,GameColor);
+				
+			}//end if 7 game move 
+			
+			
+			
+		}//end try 
+		
+		catch (InCheckException e){
+			Gamecounter--;
+			System.out.println("Please try again ,cant do that move");
+			continue;}
+		catch(ImproperColorException e){
+			Gamecounter--;
+			System.out.println("Please try again ,cant do that move");
+			continue;}
+	catch (BlockedMoveException e){
+		Gamecounter--;
+		System.out.println("Please try again ,cant do that move");
+		continue;}
+		catch(IllegalMoveException e){
+			Gamecounter--;
+			System.out.println("Please try again ,cant do that move");
+			continue;}
+		//catch anything else not planned for 
+		catch (Exception e){
+			
+			System.out.println("\nNo Moves ");
+			
+			}
+		
+		System.out.println();
+		TestBoard.updateProtectedSquares(newGame);
+		TestBoard.inCheck(newGame,gameColor);
+		if (TestBoard.getCheckStatus()){System.out.println(" Check ");}
+		
+		
+		
+			
+		
+		
+		
+		
+	}//end of while
+	
+	
+	}//end of main 
+	
 
 }
---
+++ Chess84/src/ChessGame/GameMoves.java
@@ -0,0 +1,87 @@
+package ChessGame;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import game.TestBoard;
+import gamePieces.IllegalMoveException;
+
+public class GameMoves extends ChessGame {
+
+	
+	
+	
+	
+	public static int[] convertMove(String temp) throws Exception, IllegalMoveException{
+		int[] result = new int[4];
+		if (resign && temp.equalsIgnoreCase("draw")) {
+			TestBoard.setEnd();
+			System.out.println("Draw.");
+			throw new Exception();
+		}
+		if (temp.length() == 11) {
+			String quit = temp.substring(6);
+			if (quit.equals("draw?")) {
+				resign = true;
+			}
+		}
+		else if(temp.length() != 5){
+			throw new IllegalMoveException();
+		}
+		for(String s: temp.split(" ")){
+			if(s.length() != 2 && !s.equals("draw?")){
+				throw new IllegalMoveException();
+			}
+		}
+		String regex = "[a-h][1-8]";
+		Pattern r = Pattern.compile(regex);
+		Matcher m = r.matcher(temp);
+		for(int i = 0; i <= 2; i+=2) {
+			if(m.find()) {
+				String output = m.group(0);
+				result[i] = 8 - Integer.parseInt(output.charAt(1) + "");
+				result[i + 1] = output.charAt(0) - 'a';
+			} else {
+				throw new IllegalMoveException();
+			}
+		}
+		return result;
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
